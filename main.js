227b243b707fb8e514f6e28eb5897b2a593897b2
meadchen <anastasi.prokopets@gmail.com>
2021-06-08 17:38:57 +0300

    Update main.js
    Хотела уточнить, нужно ли функции, которые используются как методы объектов, писать до объявления объектов? Или это не очень важно?

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const player1 = {
   weapon: ['sword', 'knife', 'gun'],
   attack: function () {
     console.log(player1.name + 'fight');
-  }
+  },
+  changeHP: changeHP,
+  elHP: elHP,
+  renderHP: renderHP
 }
 
 const player2 = {
@@ -20,10 +23,13 @@ const player2 = {
   weapon: ['sword', 'knife', 'gun'],
   attack: function () {
     console.log(player2.name + 'fight');
-  }
+  },
+  changeHP: changeHP,
+  elHP: elHP,
+  renderHP: renderHP
 }
 
-const createElement = (tag, className) =>{
+function createElement (tag, className){
   const createdTag = document.createElement(tag);
   if(className){
     createdTag.classList.add(className)
@@ -31,7 +37,7 @@ const createElement = (tag, className) =>{
   return createdTag;
 }
 
-const createPlayer = (playerObj) => {
+function createPlayer (playerObj){
   const player = createElement('div', 'player' + playerObj.player);
   const progressbar = createElement('div', 'progressbar');
   const character = createElement('div', 'character');
@@ -52,44 +58,80 @@ const createPlayer = (playerObj) => {
   return player;
 }
 
-const changeHP = (player) => {
-  const playerLife = document.querySelector(`.player${player.player} .life`);
-  playerLife.style.width = randomHpSubtract(player) + '%';
- 
-  if(player.hp <= 0 && player.player == 1){
-    getWinner(player, player2, playerLife);
-  } else if(player.hp <= 0 && player.player == 2){
-    getWinner(player, player1, playerLife);
+function changeHP (num) {
+  this.hp -= getRandom(num);
+  if(this.hp <= 0){
+    this.hp = 0;
   }
 }
 
+function elHP (){
+  return document.querySelector(`.player${this.player} .life`);
+}
+
+function renderHP (){
+  this.elHP().style.width = this.hp + '%';
+}
 
 
-
-function getWinner (losePlayer, winPlayer, playerLife = false) {
-  arenas.appendChild(playerWins(winPlayer.name));
-  losePlayer.hp = 0;
-  playerLife.style.width = '0%';
-  randomButton.disabled = true;
+function getWinner (winPlayer) {
   const winner = document.querySelector(`.player${winPlayer.player} .character`);
   winner.classList.add('winner');
 }
 
-const randomHpSubtract = (player) =>{
-  return player.hp -= Math.ceil(Math.random() * 20);
+function getRandom (num){
+  return Math.ceil(Math.random() * num);
 }
 
-const playerWins = (name) => {
+function playerWins (name){
   const winTitle = createElement('div', 'winTitle');
-  winTitle.innerText = name + ' wins';
+  if (name){
+    winTitle.innerText = name + ' wins';
+  } else{
+    winTitle.innerText = 'draw';
+  }
 
   return winTitle;
 }
 
 randomButton.addEventListener('click', function(){
-  changeHP(player1);
-  changeHP(player2);
+  player1.changeHP(20);
+  player2.changeHP(20);
+  player1.renderHP();
+  player2.renderHP();
+  
+
+  if (player1.hp === 0 || player2.hp === 0){
+    randomButton.disabled = true;
+    createReloadButton();
+
+    const restartButton = document.querySelector('.reloadWrap .button');
+
+    restartButton.addEventListener('click', function(){
+      window.location.reload();
+    })
+  }
+
+  if (player1.hp === 0 && player1.hp < player2.hp){
+    arenas.appendChild(playerWins(player2.name));
+    getWinner(player2);
+  } else if(player2.hp === 0 && player2.hp < player1.hp){
+    arenas.appendChild(playerWins(player1.name));
+    getWinner(player1);
+  } else if(player2.hp === 0 && player2.hp === player1.hp){
+    arenas.appendChild(playerWins());
+  }
 });
+
+
+function createReloadButton (){
+  const reloadWrap = createElement('div', 'reloadWrap');
+  const button = createElement('button', 'button');
+  reloadWrap.appendChild(button);
+  button.innerText = 'Restart';
+  arenas.appendChild(reloadWrap);
+}
+
 
 arenas.appendChild(createPlayer(player1));
 arenas.appendChild(createPlayer(player2));
