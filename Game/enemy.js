8180b3f4be0c5ae44e1f8d65d703cf559d013261
meadchen <anastasi.prokopets@gmail.com>
2021-06-22 16:18:18 +0300

    Update enemy.js

--- Game/enemy.js
+++ Game/enemy.js
@@ -1,7 +1,7 @@
 
 class Enemy{
     getEnemy = async() =>{
-        const body = fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose')
+        const body = await fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose')
             .then (res => res.json());
         return body;
     }
